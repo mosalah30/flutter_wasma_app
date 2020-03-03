116ebfcc99c901330cc08cac051c4b4a3b018b2d
moha <msmlbm30@gmail.com>
2020-03-03 21:22:31 +0200

    added theme to project

---
+++ lib/UI/main_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  static final String id='MainScreen';
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static ThemeData kDefaultTheme = ThemeData(
+     backgroundColor : Color(0xffF4F2F3),
+      primaryColor: Color(0xffD3B88C),
+      accentColor: Color(0xff656256));
+}
